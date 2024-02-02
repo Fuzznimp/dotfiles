ad8afd275eb5c89925e5d360cbb20faed79baeac
Fuzznimp <montaigne.anthony.dev@gmail.com>
2024-02-02 14:50:05 +0100

    chore(dotfiles): backup before removing mackup

--- mackup/.config/nvim/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-
-require("notify").setup({
-  background_colour = "#000000",
-})
-
--- vim.cmd.colorscheme("catppuccin")
