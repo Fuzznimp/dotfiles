621dfe23fdfa974017f2b44f7ee7636213fa8d1f
Fuzznimp <anthony@montaigne.dev>
2026-03-24 10:02:17 +0100

    feat(raycast): custom scripts

---
+++ .config/raycast/scripts/dp1-reset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title DP1 Reset
+# @raycast.mode silent
+# @raycast.packageName Custom
+
+open "raycast://extensions/eluce2/displayplacer/preset1"
+sleep 0.5
+open "raycast://extensions/raycast/raycast/reset-raycast-window-position"
---
+++ .config/raycast/scripts/dp2-reset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.title DP2 Reset
+# @raycast.mode silent
+# @raycast.packageName Custom
+
+open "raycast://extensions/eluce2/displayplacer/preset2"
+sleep 0.5
+open "raycast://extensions/raycast/raycast/reset-raycast-window-position"
